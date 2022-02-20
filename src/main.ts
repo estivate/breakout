e47bfc7316e3e6e9dbe60aaf2c00e2a8296cf258
Blake Ellis <blake@commercev3.com>
2022-02-20 16:33:19 -0500

    playing with resolutions

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ if (canvas == null) {
   throw new Error("No Canvas element in DOM to attache to.");
 }
 
+//canvas.width = innerWidth;
+//canvas.height = innerHeight;
+
 let ctx = canvas.getContext("2d")!;
 
 let x = canvas.width / 2;
@@ -95,10 +98,10 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-function mouseMoveHandler(e: { clientX: number; }) {
+function mouseMoveHandler(e: { clientX: number }) {
   let relativeX = e.clientX - canvas.offsetLeft;
-  if(relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth/2;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
   }
 }
 
@@ -131,18 +134,17 @@ function checkPaddleCollide() {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
     } else {
-      lives --;
+      lives--;
       if (!lives) {
         alert("Game Over");
         document.location.reload();
       } else {
-        x = canvas.width/2;
-        y = canvas.height-30;
+        x = canvas.width / 2;
+        y = canvas.height - 30;
         dx = ballSpeed;
         dy = -ballSpeed;
-        paddleX = (canvas.width-paddleWidth)/2;
+        paddleX = (canvas.width - paddleWidth) / 2;
       }
-
     }
   }
 
@@ -163,30 +165,34 @@ function checkBrickCollide() {
   for (var c = 0; c < brickColumnCount; c++) {
     for (var r = 0; r < brickRowCount; r++) {
       var b = bricks[c][r];
-      if (b.status == 1){
-        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
           dy = -dy;
           b.status = 0;
           score++;
-          if(score == brickRowCount*brickColumnCount) {
+          if (score == brickRowCount * brickColumnCount) {
             alert("YOU WIN! CONGRATULATIONS!");
             document.location.reload();
           }
         }
       }
-
     }
   }
 }
 
 function drawScore() {
-  ctx.font="16px Ariel";
-  ctx.fillStyle="#0095DD";
+  ctx.font = "16px Ariel";
+  ctx.fillStyle = "#0095DD";
   ctx.fillText("Score: " + score, 8, 20);
 }
 
 function drawLifeCounter() {
-  ctx.font="16px Ariel";
-  ctx.fillStyle="#0095DD";
-  ctx.fillText("Lives: " + lives, canvas.width-65, 20);
+  ctx.font = "16px Ariel";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
 }
